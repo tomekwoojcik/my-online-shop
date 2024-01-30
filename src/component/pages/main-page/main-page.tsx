436270516bc95c74dc9a0670415713e7752e71e4
tomekwoojcik <94222884+tomekwoojcik@users.noreply.github.com>
2024-01-30 19:31:59 +0100

    fix develop footer component

--- src/component/pages/main-page/main-page.tsx
+++ src/component/pages/main-page/main-page.tsx
@@ -2,12 +2,19 @@ import { FC } from "react";
 import { Navbar } from "../../organism/navbar/navbar";
 import { NavbarMenuProvider } from "../../../context/navbar-menu-context";
 import BoxSliderMenu from "../../organism/box-slider-menu/box-slider-menu";
+import Footer from "../../organism/footer/footer";
+import { FooterProvider } from "../../../context/footerContext";
 
 export const MainPage: FC = () => {
   return (
+    <>
       <NavbarMenuProvider>
         <Navbar />
-        <BoxSliderMenu/>
+        <BoxSliderMenu />
       </NavbarMenuProvider>
+      <FooterProvider>
+        <Footer />
+      </FooterProvider>
+    </>
   );
 };
