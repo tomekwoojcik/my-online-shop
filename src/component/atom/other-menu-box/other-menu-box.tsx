439faaa0b3c8e84d43e8b10f939982dbf0719d7d
tomekwoojcik <94222884+tomekwoojcik@users.noreply.github.com>
2024-02-11 09:36:43 +0100

    fix bug

--- src/component/atom/other-menu-box/other-menu-box.tsx
+++ src/component/atom/other-menu-box/other-menu-box.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { Box } from "@mui/material";
-import { ArrButtonModel } from "../../../context/footer-context";
+import { ArrButtonModel } from "../../../state/state";
 import HeadingFooter from "../heading-footer/heading-footer";
 import { ButtonCustomize, ListCustomize } from "./other-menu-customize";
 
