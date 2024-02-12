240493212d01ac025b4a3bfdf7b9b0af8d72be18
tomekwoojcik <94222884+tomekwoojcik@users.noreply.github.com>
2024-02-12 18:50:43 +0100

    main

--- src/component/pages/main-page/main-page.tsx
+++ src/component/pages/main-page/main-page.tsx
@@ -4,6 +4,7 @@ import { NavbarMenuProvider } from "../../../context/navbar-menu-context";
 import BoxSliderMenu from "../../organism/box-slider-menu/box-slider-menu";
 import { FooterProvider } from "../../../context/footer-context";
 import Footer from "../../organism/footer/footer";
+import MainPageTemplate from "../../template/main-page";
 
 export const MainPage: FC = () => {
   return (
@@ -12,6 +13,7 @@ export const MainPage: FC = () => {
         <Navbar />
         <BoxSliderMenu />
       </NavbarMenuProvider>
+      <MainPageTemplate/>
       <FooterProvider>
         <Footer />
       </FooterProvider>
---
+++ src/component/template/main-page.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import styled from "styled-components";
+
+const BoxSlider = styled(Box)`
+
+`
+
+const MainPageTemplate = () => {
+    return (
+        <BoxSlider>
+
+        </BoxSlider>
+    )
+}
+export default MainPageTemplate;
